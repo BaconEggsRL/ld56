3ca72ea48ba8d7eb304b3263fefca894f84eca0f
BaconEggsRL <apatton090@gmail.com>
2024-10-04 20:23:21 -0400

    movement

--- entities/friend.gd
+++ entities/friend.gd
@@ -4,13 +4,59 @@ extends CharacterBody2D
 var SPEED = 300.0
 var JUMP_VELOCITY = -400.0
 var FOLLOW_SPEED = 4.0
+var AURA_LERP_SPEED = 4.0
 
 @export var player : CharacterBody2D
 @onready var following_player : bool = true
 
+@onready var collect_aura = $collect_aura
+@onready var collect_area = $collect_area
+@onready var collect_area_collision = collect_area.get_node("CollisionShape2D")
+
 var y_offset = 10
 
 
+@onready var collected : bool = false
+signal friend_collected
+var lerp_color = false
+var new_color = Color(1,1,1,0)
+
+
+@onready var line_node: Line2D = get_node("Line2D")
+@onready var start_point: Marker2D = get_node("Marker2D")
+
+const MAX_TIME := 20. # Sec
+const TIME_STEP := 0.1 # Sec
+const GRAV_ACC := 9.8 # Meters/Sec^2
+
+
+# theta in radians, otherwise use deg_to_rad() before-hand
+func solve_path(v_0: float, theta: float):
+	var pts := PackedVector2Array([])
+	var t: float = 0.
+	var x: float = start_point.global_position.x
+	var y: float = start_point.global_position.y
+
+	# Godot y-axis is flipped
+	var v_x: float = v_0 * cos(theta)
+	var v_y: float = -v_0 * sin(theta)
+
+	# Very crude algorithm, specifically this is the "Euler method"
+	# For more sophesticated situations (ie > 1 projectile interacting with each other)
+	# use "Runge-Kutta Methods" or "Verlet Integration Methods"
+	while t < MAX_TIME: # No infinities
+		v_y += GRAV_ACC * TIME_STEP
+		v_x += 0 # No X Forces (no drag)
+		t += TIME_STEP
+		x += v_x * TIME_STEP
+		y += v_y * TIME_STEP
+		pts.push_back(Vector2(x, y))
+		if y > 400: # hit the ground
+			break
+	line_node.points = pts
+	return
+	
+	
 
 func _on_changed_control(player_controlled):
 	self.following_player = player_controlled
@@ -28,32 +74,55 @@ func _ready() -> void:
 	self.SPEED = player.SPEED
 	self.JUMP_VELOCITY = player.JUMP_VELOCITY
 	player.changed_control.connect(_on_changed_control)
+	# solve_path(50, deg_to_rad(30))
 
 
 func _physics_process(delta: float) -> void:
 	
+	if lerp_color:
+		self.collect_aura.color = self.collect_aura.color.lerp(new_color, delta * AURA_LERP_SPEED)
+		if self.collect_aura.color == new_color:
+			lerp_color = false
+			collect_aura.hide()
 	
-	if not following_player:
-		# Add the gravity.
-		if not is_on_floor():
-			velocity += get_gravity() * delta
+	if collected:
+	
+		if not following_player:
+			# Add the gravity.
+			if not is_on_floor():
+				velocity += get_gravity() * delta
 
-		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+			# Handle jump.
+			if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
 
-		# Get the input direction and handle the movement/deceleration.
-		# As good practice, you should replace UI actions with custom gameplay actions.
-		var direction := Input.get_axis("ui_left", "ui_right")
-		if direction:
-			velocity.x = direction * SPEED
+			# Get the input direction and handle the movement/deceleration.
+			# As good practice, you should replace UI actions with custom gameplay actions.
+			var direction := Input.get_axis("ui_left", "ui_right")
+			if direction:
+				velocity.x = direction * SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+
+			move_and_slide()
+			
 		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+			var player_pos = player.position
+			var follow_pos = player_pos + Vector2(0, -y_offset*10)
+			self.position = self.position.lerp(follow_pos, delta * FOLLOW_SPEED)
+		
 
-		move_and_slide()
+
+func _on_collect_area_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body.is_in_group("player"):
+		collect_area_collision.call_deferred("set", "disabled", true)
+		# $Sound.play()
+		collect_area.hide()
+		friend_collected.connect(player._on_friend_collected)
+		friend_collected.emit(self)
+		self.collected = true
 		
-	else:
-		var player_pos = player.position
-		var follow_pos = player_pos + Vector2(0, -y_offset*10)
-		self.position = self.position.lerp(follow_pos, delta * FOLLOW_SPEED)
-		
+		# print_debug(self.collect_aura.color)
+		lerp_color = true
+		# collect_aura.hide()
--- entities/player.gd
+++ entities/player.gd
@@ -8,18 +8,48 @@ const JUMP_VELOCITY = -400.0
 signal changed_control
 
 
+@onready var num_friends_collected = 0
+@onready var friends_available : Array[CharacterBody2D] = []
+
+@onready var charging_throw : bool = false
+
+
+func _on_friend_collected(friend) -> void:
+	# print_debug("collected")
+	# print_debug(friend)
+	num_friends_collected += 1
+	friends_available.append(friend)
+	# print_debug(friends_available)
+
+
+
+func _ready() -> void:
+	pass
+
+
 func _physics_process(delta: float) -> void:
+
 	
 	# Check if changing input
-	if Input.is_action_just_pressed("change_control") and is_on_floor():
-		player_controlled = not player_controlled
-		changed_control.emit(player_controlled)
+	if friends_available.size() > 0:
+		if Input.is_action_just_pressed("change_control"): # and is_on_floor():
+			player_controlled = not player_controlled
+			changed_control.emit(player_controlled)
 	
 	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+	# Check if charging throw
+	if player_controlled:
+		if Input.is_action_just_pressed("throw"):
+			if Input.is_action_pressed("charge_throw"):
+				print("throw")
+			else:  
+				print("do nothing")
+		else:
+			if Input.is_action_pressed("charge_throw"):
+				charging_throw = true
+			
+	
+	
 	if player_controlled:
 		# Handle jump.
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -36,4 +66,9 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+		# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+		
 	move_and_slide()
