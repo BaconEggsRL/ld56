611a5a03671b26b1dd9bb92d2628417fd670610b
BaconEggsRL <apatton090@gmail.com>
2024-10-04 21:57:44 -0400

    thrown

--- entities/friend.gd
+++ entities/friend.gd
@@ -17,25 +17,49 @@ var y_offset = 10
 
 
 @onready var collected : bool = false
+@onready var thrown : bool = false
+@onready var available : bool = false
+
+
 signal friend_collected
+signal friend_available
+
 var lerp_color = false
-var new_color = Color(1,1,1,0)
+var COLOR_TRANSPARENT = Color(1,1,1,0)
+var COLOR_COLLECTABLE = Color(1,1,0,0.50)
+var new_color = COLOR_COLLECTABLE
 
 
 @onready var line_node: Line2D = get_node("Line2D")
 @onready var start_point: Marker2D = get_node("Marker2D")
 
-const MAX_TIME := 20. # Sec
+const MAX_TIME := 12. # Sec
 const TIME_STEP := 0.1 # Sec
 const GRAV_ACC := 9.8 # Meters/Sec^2
 
+var THROW_XDAMP_SPEED = 6.0
+var THROW_VELOCITY : Vector2 = Vector2(500.0, -500.0)
 
+
+
+func draw_trajectory():
+	var mouse_pos : Vector2 = get_local_mouse_position()
+	var start_pos : Vector2 = Vector2(start_point.position.x, start_point.position.y)
+	var launch_vector : Vector2 = mouse_pos - start_pos
+	var floor_vector : Vector2 = Vector2(mouse_pos.x, start_point.position.y)
+	var dot_product = launch_vector.dot(floor_vector)
+	var expression = dot_product / (launch_vector.length() * floor_vector.length())
+	var theta = acos(expression)
+	# print(rad_to_deg(theta))
+	solve_path(mouse_pos.x - start_pos.x, theta)
+	
+	
 # theta in radians, otherwise use deg_to_rad() before-hand
 func solve_path(v_0: float, theta: float):
 	var pts := PackedVector2Array([])
 	var t: float = 0.
-	var x: float = start_point.global_position.x
-	var y: float = start_point.global_position.y
+	var x: float = start_point.position.x
+	var y: float = start_point.position.y
 
 	# Godot y-axis is flipped
 	var v_x: float = v_0 * cos(theta)
@@ -56,6 +80,26 @@ func solve_path(v_0: float, theta: float):
 	line_node.points = pts
 	return
 	
+
+
+func _on_thrown(_friend):
+	
+	self.available = false
+
+	new_color = COLOR_COLLECTABLE
+	# collect_aura.color = new_color
+	collect_area_collision.call_deferred("set", "disabled", false)
+	collect_aura.show()
+	
+	self.velocity = THROW_VELOCITY
+	
+	self.thrown = true
+	self.following_player = false
+	if self.following_player:
+		self.collision_layer = 0
+	else:
+		self.collision_layer = 2
+
 	
 
 func _on_changed_control(player_controlled):
@@ -64,7 +108,6 @@ func _on_changed_control(player_controlled):
 		self.collision_layer = 0
 	else:
 		self.collision_layer = 2
-		print(self.collision_layer)
 		
 	
 
@@ -74,7 +117,10 @@ func _ready() -> void:
 	self.SPEED = player.SPEED
 	self.JUMP_VELOCITY = player.JUMP_VELOCITY
 	player.changed_control.connect(_on_changed_control)
+	self.collect_aura.color = COLOR_COLLECTABLE
 	# solve_path(50, deg_to_rad(30))
+	friend_collected.connect(player._on_friend_collected)
+	friend_available.connect(player._on_friend_available)
 
 
 func _physics_process(delta: float) -> void:
@@ -84,45 +130,76 @@ func _physics_process(delta: float) -> void:
 		if self.collect_aura.color == new_color:
 			lerp_color = false
 			collect_aura.hide()
-	
+
 	if collected:
 	
 		if not following_player:
 			# Add the gravity.
 			if not is_on_floor():
 				velocity += get_gravity() * delta
+				
+			if not thrown:	
 
-			# Handle jump.
-			if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-				velocity.y = JUMP_VELOCITY
+				# Handle jump.
+				if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+					velocity.y = JUMP_VELOCITY
 
-			# Get the input direction and handle the movement/deceleration.
-			# As good practice, you should replace UI actions with custom gameplay actions.
-			var direction := Input.get_axis("ui_left", "ui_right")
-			if direction:
-				velocity.x = direction * SPEED
+				# Get the input direction and handle the movement/deceleration.
+				# As good practice, you should replace UI actions with custom gameplay actions.
+				var direction := Input.get_axis("ui_left", "ui_right")
+				if direction:
+					velocity.x = direction * SPEED
+				else:
+					velocity.x = move_toward(velocity.x, 0, SPEED)
+				
 			else:
-				velocity.x = move_toward(velocity.x, 0, SPEED)
+				velocity.x = move_toward(velocity.x, 0, THROW_XDAMP_SPEED)
+
 
 			move_and_slide()
 			
 		else:
-			var player_pos = player.position
-			var follow_pos = player_pos + Vector2(0, -y_offset*10)
-			self.position = self.position.lerp(follow_pos, delta * FOLLOW_SPEED)
+			
+			if not thrown:
+			
+				var player_pos = player.position
+				var follow_pos = player_pos + Vector2(0, -y_offset*10)
+				self.position = self.position.lerp(follow_pos, delta * FOLLOW_SPEED)
+				
+			else:
+				
+				if not is_on_floor():
+					velocity += get_gravity() * delta
+				
 		
 
 
 func _on_collect_area_body_entered(body: Node2D) -> void:
-	print(body.name)
+	# print(body.name)
 	if body.is_in_group("player"):
-		collect_area_collision.call_deferred("set", "disabled", true)
-		# $Sound.play()
-		collect_area.hide()
-		friend_collected.connect(player._on_friend_collected)
-		friend_collected.emit(self)
-		self.collected = true
-		
-		# print_debug(self.collect_aura.color)
-		lerp_color = true
-		# collect_aura.hide()
+		if self.collected == false:
+			collect_area_collision.call_deferred("set", "disabled", true)
+			# $Sound.play()
+			collect_area.hide()
+			# friend_collected.connect(player._on_friend_collected)
+			friend_collected.emit(self)
+			self.collected = true
+			self.available = true
+			
+			# print_debug(self.collect_aura.color)
+			new_color = COLOR_TRANSPARENT
+			lerp_color = true
+			# collect_aura.hide()
+			
+		elif self.available == false:
+			collect_area_collision.call_deferred("set", "disabled", true)
+			# $Sound.play()
+			collect_area.hide()
+			# friend_available.connect(player._on_friend_available)
+			friend_available.emit(self)
+			self.available = true
+			
+			# print_debug(self.collect_aura.color)
+			new_color = COLOR_TRANSPARENT
+			lerp_color = true
+			# collect_aura.hide()
--- entities/player.gd
+++ entities/player.gd
@@ -6,12 +6,19 @@ const JUMP_VELOCITY = -400.0
 
 @onready var player_controlled = true
 signal changed_control
+signal thrown
 
 
 @onready var num_friends_collected = 0
 @onready var friends_available : Array[CharacterBody2D] = []
+# first friend is out by default. (element 0)
+# friends are appended to the end of the array as they come in. (last element)
+# and exit from the front of the array as they leave (element 0).
 
 @onready var charging_throw : bool = false
+
+@export var num_friends_collected_label : Label
+@export var num_friends_available_label : Label
 
 
 func _on_friend_collected(friend) -> void:
@@ -20,11 +27,39 @@ func _on_friend_collected(friend) -> void:
 	num_friends_collected += 1
 	friends_available.append(friend)
 	# print_debug(friends_available)
+	
+	# num friends collected
+	var collected_string = "%s / 10" % str(num_friends_collected)
+	self.num_friends_collected_label.text = collected_string
+	
+	# num friends available
+	var available_string = "%s / %s" % [str(friends_available.size()), str(num_friends_collected)]
+	self.num_friends_available_label.text = available_string
+	
+	
+
+func _on_friend_available(friend) -> void:
+	# print_debug("collected")
+	# print_debug(friend)
+	friends_available.append(friend)
+	
+	#print(friend.lerp_color)
+	#print(friend.collected)
+	#print(friend.following_player)
+	#print(friend.thrown)
+	
+	friend.following_player = true
+	friend.thrown = false
+	# print_debug(friends_available)
+	
+	# num friends available
+	var available_string = "%s / %s" % [str(friends_available.size()), str(num_friends_collected)]
+	self.num_friends_available_label.text = available_string
 
 
 
 func _ready() -> void:
-	pass
+	self.num_friends_collected_label.text = "0 / 10"
 
 
 func _physics_process(delta: float) -> void:
@@ -37,16 +72,45 @@ func _physics_process(delta: float) -> void:
 			changed_control.emit(player_controlled)
 	
 	
-	# Check if charging throw
-	if player_controlled:
-		if Input.is_action_just_pressed("throw"):
-			if Input.is_action_pressed("charge_throw"):
-				print("throw")
-			else:  
-				print("do nothing")
-		else:
-			if Input.is_action_pressed("charge_throw"):
-				charging_throw = true
+		# Check if charging throw
+		if player_controlled:
+			if Input.is_action_just_pressed("throw"):
+				if Input.is_action_pressed("charge_throw"):
+					print("throw")
+					var friend = friends_available[0]
+					var traj = friend.get_node("Line2D")
+					traj.hide()
+					
+					thrown.connect(friend._on_thrown)
+					thrown.emit(friend)
+					thrown.disconnect(friend._on_thrown)
+					
+					# num friends available
+					friends_available.remove_at(0)
+					var available_string = "%s / %s" % [str(friends_available.size()), str(num_friends_collected)]
+					self.num_friends_available_label.text = available_string
+	
+	
+				else:  
+					print("do nothing")
+					
+			else:
+				
+				var friend = friends_available[0]
+				var traj = friend.get_node("Line2D")
+				
+				if Input.is_action_pressed("charge_throw"):
+					
+					
+					friend.draw_trajectory()
+					
+					charging_throw = true
+					traj.show()
+				else:
+					traj.hide()
+
+					
+					
 			
 	
 	
